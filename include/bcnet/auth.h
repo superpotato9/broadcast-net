23038478fd76666bb31f1a5b03912440e1891013
nathan <nkoliha@gmail.com>
2022-11-25 14:25:30 -0800

    auth/main.c just got stfu

--- include/bcnet/auth.h
+++ include/bcnet/auth.h
@@ -4,9 +4,8 @@
 
 #ifndef BROADCAST_NET_AUTH_H
 #define BROADCAST_NET_AUTH_H
-int RSA_public_encrypt(int flen, unsigned char *from,
-                       unsigned char *to, RSA *rsa, int padding);
-int RSA_private_decrypt(int flen, unsigned char *from,
-                        unsigned char *to, RSA *rsa, int padding);
+//int RSA_public_encrypt(int flen, unsigned char *from,
+//                       unsigned char *to, RSA *rsa, int padding);
+
 #endif //BROADCAST_NET_AUTH_H
 
--- tests/test.c
+++
@@ -1,8 +0,0 @@
-    #include <stdio.h>
-#include <openssl/rsa.h>
-
-
-
-int main(){
-
-}
