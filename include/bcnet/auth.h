06f9ba985d479feb471731b54e81dbb17e2c8d84
FreehandBlock51 <60456293+FreehandBlock51@users.noreply.github.com>
2022-12-08 15:27:17 -0800

    changed key_t to char[256] for ease of use

--- include/bcnet/auth.h
+++ include/bcnet/auth.h
@@ -8,23 +8,18 @@
 #define BROADCAST_NET_AUTH_H
 
 // a public or private key
-typedef struct Key {
-    // the key string
-    char* keystr;
-    // the length of the key string
-    size_t keylen;
-} key_t;
+typedef char key_t[256];
 
 // takes public key and  message and returns the rsa encryted message
-char* rsa_encrypt(key_t pub_key, char *msg, size_t msglen);
+int rsa_encrypt(key_t pub_key, char *msg, size_t msglen, char *encrypted, size_t encryptedlen);
 
 // take private key and encrypted message and returns rsa decrypted message
-char* rsa_decrypt(key_t priv_key, char *msg, size_t msglen);
+int rsa_decrypt(key_t priv_key, char *msg, size_t msglen, char *decrypted, size_t decryptedlen);
 
 // encrypts message using TBD protocol
-char* encrypt(key_t key, char *msg, size_t msglen);
+int encrypt(key_t key, char *msg, size_t msglen, char *encrypted, size_t encryptedlen);
 
 // decrypts message using TBD protocol 
-char* decrypt(key_t priv_key, char *msg, size_t msglen);
+int decrypt(key_t priv_key, char *msg, size_t msglen, char *decrypted, size_t decryptedlen);
 #endif //BROADCAST_NET_AUTH_H
 
