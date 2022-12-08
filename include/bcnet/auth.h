4d3b21a1729b6f5ac5743e867c8fbcd087c0d159
nathan <50345854+superpotato9@users.noreply.github.com>
2022-12-08 15:01:44 -0800

    Update auth.h

--- include/bcnet/auth.h
+++ include/bcnet/auth.h
@@ -4,8 +4,13 @@
 
 #ifndef BROADCAST_NET_AUTH_H
 #define BROADCAST_NET_AUTH_H
+
 //int RSA_public_encrypt(int flen, unsigned char *from,
 //                       unsigned char *to, RSA *rsa, int padding);
+char* rsa_encrypt(char *pub_key, char *msg); // takes public key and  message and returns the rsa encryted message
+char* rsa_decrypt(char *priv_key, char *msg); // take priv key and encrypted message and returns rsa decrypted message
 
+char* encrypt(char *key, char *msg); // encrypts message using TBD protocol
+char* decrypt(char *priv_key, char *msg); // decrypts message using TBD protocol 
 #endif //BROADCAST_NET_AUTH_H
 
