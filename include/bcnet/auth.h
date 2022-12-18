70d158c2288e1ecc19af04bf5313c8c8038833e3
FreehandBlock51 <60456293+FreehandBlock51@users.noreply.github.com>
2022-12-18 15:12:43 -0800

    implemented connect_to() (#1)
    Implemented the connect_to() function, refactored out packet creation into an internal function, added some utilities to auth.h, and moved definitions not used by the end user from bcnet.h to bcnet.c

--- include/bcnet/auth.h
+++ include/bcnet/auth.h
@@ -7,14 +7,26 @@
 #ifndef BROADCAST_NET_AUTH_H
 #define BROADCAST_NET_AUTH_H
 
-// a public or private key
-typedef char bcn_key_t[256];
+// the length of a key, in bytes
+#define BCN_KEY_LEN 256
 
-// takes public key and  message and returns the rsa encryted message
+// a public or private key
+typedef char bcn_key_t[BCN_KEY_LEN];
+
+// assigns a value to a key
+#define bcn_assign_key(src, dest) memcpy(dest, src, sizeof(bcn_key_t))
+
+// creates a public-private RSA keypair
+int rsa_create_keypair(unsigned int prime1, unsigned int prime2, bcn_key_t public, bcn_key_t private);
+
+// takes public key and message and returns the rsa encryted message
 int rsa_encrypt(bcn_key_t pub_key, char *msg, size_t msglen, char *encrypted, size_t encryptedlen);
 
 // take private key and encrypted message and returns rsa decrypted message
 int rsa_decrypt(bcn_key_t priv_key, char *msg, size_t msglen, char *decrypted, size_t decryptedlen);
+
+// creates a key using TBD protocol
+int create_key(bcn_key_t key);
 
 // encrypts message using TBD protocol
 int encrypt(bcn_key_t key, char *msg, size_t msglen, char *encrypted, size_t encryptedlen);
--- src/bcnet/bcnet.c
+++ src/bcnet/bcnet.c
@@ -17,6 +17,54 @@
 #include <string.h>
 #include <stdint.h>
 
+
+/* Internal Definitions
+ * These are necessary for the implementations of functions,
+ * but are not used by the end user.  We put them here to
+ * keep bcnet.h small.
+ */
+// the constant
+#define BCN_CONST "This is a test constant"
+#define BCN_CONST_LEN sizeof(BCN_CONST)
+
+// the type of a BNP packet
+enum PacketType
+{
+  CONTENT,
+  SETUP,
+  STREAM
+};
+
+// the header of a BNP packet
+struct bcn_packet_header {
+  char constant[BCN_CONST_LEN]; // the constant
+  enum PacketType type; // the packet's type
+  uint8_t order; // unused unless packet is of type STREAM; if it is, then the amount of STREAM packets sent before this one
+  uint32_t content_length; // the length of the content, in bytes
+};
+
+// populates a BNP packet
+int bcn_populate_packet(char *packet, size_t packetsize, enum PacketType type, uint8_t order, uint32_t content_length, char* content) {
+  if (packetsize < sizeof(struct bcn_packet_header) + (content_length * 8)) { // buffer for packet is too small
+    errno = EINVAL;
+    return -1;
+  }
+
+  struct bcn_packet_header *header = (struct bcn_packet_header*)packet;
+  *header = (struct bcn_packet_header){
+    .constant = BCN_CONST,
+    .type = type,
+    .order = order,
+    .content_length = content_length
+  };
+  char *body = packet + sizeof(struct bcn_packet_header);
+  memcpy(body, content, content_length * 8); // multiply content_length by 8 becuase it specifies length in bytes, but we need length in bits
+  return 0;
+}
+
+/* Function Implementations
+ * These are implementations of functions defined in bcnet.h
+ */
 bcn_socket_t create_socket() {
   bcn_socket_t bcn_socket = (bcn_socket_t){ 0 };
   bcn_socket.fd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -38,26 +86,28 @@ int send_content(bcn_socket_t* bcn_socket, char* buffer, int32_t bufSize, struct
   if (bcn_socket->streamPacketsSent > 0) { // stream sockets sending content packets will lose their connection to the stream
     bcn_socket->streamPacketsSent = 0;
     memset(bcn_socket->convKey, 0, sizeof(bcn_key_t));
-    memset(bcn_socket->curTarget, 0, sizeof(bcn_key_t));
+    memset(&bcn_socket->curTarget, 0, sizeof(struct sockaddr_bcn));
   }
 
+  // create the packet
   size_t packetSize = sizeof(struct bcn_packet_header) + bufSize;
-  char *packet = malloc(packetSize);
-  if (packet == NULL) {
+  char *packet;
+  char *encryptedpacket;
+  if ((packet = malloc(packetSize)) == NULL || (encryptedpacket = malloc(packetSize)) == NULL) {
+    free(packet);
+    free(encryptedpacket); // free both in case one was allocated
     return -1; // errno set by malloc
   }
-
-  // populate the header
-  struct bcn_packet_header *header = (struct bcn_packet_header*)packet;
-  *header = (struct bcn_packet_header){ .constant = BNP_CONST };
-  header->content_length = bufSize;
-  header->type = CONTENT;
-  header->order = 0;
-
-  // populate the body
-  char* body = packet + sizeof(struct bcn_packet_header);
-  if (!rsa_encrypt(target.key, buffer, bufSize, body, sizeof(body))) {
+  if (!bcn_populate_packet(packet, packetSize, CONTENT, 0, bufSize / 8, buffer)) {
     free(packet);
+    free(encryptedpacket);
+    return -1; // errno set by bcn_populate_packet
+  }
+
+  // encrypt the packet
+  if (!rsa_encrypt(target.key, packet, packetSize, encryptedpacket, packetSize)) {
+    free(packet);
+    free(encryptedpacket);
     return -1; // errno should be set by rsa_encrypt
   }
 
@@ -72,10 +122,11 @@ int send_content(bcn_socket_t* bcn_socket, char* buffer, int32_t bufSize, struct
   };
   for (uint16_t lastIpHalf = 0; lastIpHalf < UINT16_MAX; lastIpHalf++) {
     tempaddr.sin_addr.s_addr++;
-    sendto(bcn_socket->fd, packet, packetSize, 0, (struct sockaddr*)&tempaddr, sizeof(struct sockaddr));
+    sendto(bcn_socket->fd, encryptedpacket, packetSize, 0, (struct sockaddr*)&tempaddr, sizeof(struct sockaddr));
   }
 
   free(packet);
+  free(encryptedpacket);
   return 0;
 }
 
@@ -83,7 +134,7 @@ int recv_content(bcn_socket_t* bcn_socket, char* buffer, int32_t bufsize, struct
   if (bcn_socket->streamPacketsSent > 0) { // stream sockets sending content packets will lose their connection to the stream
     bcn_socket->streamPacketsSent = 0;
     memset(bcn_socket->convKey, 0, sizeof(bcn_key_t));
-    memset(bcn_socket->curTarget, 0, sizeof(bcn_key_t));
+    memset(&bcn_socket->curTarget, 0, sizeof(struct sockaddr_bcn));
   }
 
   int result = 0;
@@ -111,7 +162,7 @@ int recv_content(bcn_socket_t* bcn_socket, char* buffer, int32_t bufsize, struct
   for (uint16_t lastIpHalf = 0; lastIpHalf < UINT16_MAX; lastIpHalf++) {
     tempaddr.sin_addr.s_addr++;
     memset(encryptedpacket, 0, packetsize); // clear packet buffer to prevent corruption
-    result = recvfrom(bcn_socket->fd, encryptedpacket, packetsize, 0, (struct sockaddr*)&tempaddr, &tempaddrlen); // TODO do we need to cast tempaddr to a struct sockaddr?
+    result = recvfrom(bcn_socket->fd, encryptedpacket, packetsize, 0, (struct sockaddr*)&tempaddr, &tempaddrlen);
 
     // check if the packet is ours
     if (result < sizeof(struct bcn_packet_header)) { // packet is smaller than our header size, and therefore can't be ours
@@ -121,9 +172,14 @@ int recv_content(bcn_socket_t* bcn_socket, char* buffer, int32_t bufsize, struct
       break; // cleanup is done after the loop, & errno is set by rsa_decrypt
     }
     struct bcn_packet_header *header = (struct bcn_packet_header*)decryptedpacket;
-    if (header->constant != BNP_CONST || header->type != CONTENT) {
+    if (header->constant != BCN_CONST || header->type != CONTENT) {
       memset(decryptedpacket, 0, packetsize); // clear decrypted buffer to prevent corruption
       continue;
+    }
+    if (header->content_length * 8 > bufsize) { // buffer is too small
+      errno = EFBIG; // 'file is too big' (in our case, the file is a packet)
+      result = -1;
+      break; // cleanup is after the loop
     }
 
     // get the message content
@@ -134,8 +190,62 @@ int recv_content(bcn_socket_t* bcn_socket, char* buffer, int32_t bufsize, struct
     }
     break; // result will either be 0 or errno will be set.  Either way, cleanup is after the loop
   }
+  
+  // cleanup
   free(encryptedpacket);
   free(decryptedpacket);
 
   return result;
+}
+
+int connect_to(bcn_socket_t *bcn_socket, struct sockaddr_bcn target) {
+  // this socket is now a stream socket, so we need to initilize the corresponding values
+  memcpy(&bcn_socket->curTarget, &target, sizeof(struct sockaddr_bcn));
+  create_key(bcn_socket->convKey);
+  bcn_socket->streamPacketsSent = 0;
+
+  // create the setup packet
+  size_t packetSize = sizeof(struct bcn_packet_header) + sizeof(bcn_key_t);
+  char *packet, *encryptedPacket;
+  if ((packet = malloc(packetSize)) == NULL || (encryptedPacket = malloc(packetSize)) == NULL) { // malloc error
+    free(packet);
+    free(encryptedPacket); // free both buffers incase one malloc was successful
+    return -1; // errno is set by malloc
+  }
+  if (!bcn_populate_packet(packet, packetSize, CONTENT, 0, sizeof(bcn_key_t) / 8, bcn_socket->convKey)) {
+    free(packet);
+    free(encryptedPacket);
+    return -1; // errno set by bcn_populate_packet
+  }
+
+  // encrypt the packet
+  if (!rsa_encrypt(target.key, packet, packetSize, encryptedPacket, packetSize)) {
+    free(packet);
+    free(encryptedPacket);
+    return -1; // errno should be set by rsa_encrypt
+  }
+
+  // brodcast the packet
+  struct sockaddr_in tempaddr = {
+    .sin_family = AF_INET,
+    .sin_addr = {
+      .s_addr = target.range.full << 16
+    },
+    .sin_port = target.port,
+    .sin_zero = {0,0,0,0,0,0,0,0}
+  };
+  for (uint16_t lastIpHalf = 0; lastIpHalf < UINT16_MAX; lastIpHalf++) {
+    tempaddr.sin_addr.s_addr++;
+    sendto(bcn_socket->fd, encryptedPacket, packetSize, 0, (struct sockaddr*)&tempaddr, sizeof(struct sockaddr));
+  }
+
+  // cleanup
+  free(packet);
+  free(encryptedPacket);
+
+  return 0;
+}
+
+int accept_from(bcn_socket_t* bcn_socket) {
+
 }
